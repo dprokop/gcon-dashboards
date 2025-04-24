40998173112d42b660782dd83b7d7a1dd6cd2fe8
Dominik Prokop <dominik.prokop@grafana.com>
2025-04-24 10:36:11 +0200

    Foundation example

---
+++ foundation/src/index.ts
@@ -0,0 +1,76 @@
+import fs from 'fs';
+import path from 'path';
+import yaml from 'yaml';
+
+import { exampleDashboard } from './dashboard';
+import { Client } from './grafana';
+import { dashboardManifest } from './manifests';
+import { DashboardBuilder } from '@grafana/grafana-foundation-sdk/dashboard';
+
+const manifestsDir = './manifests';
+const dashboardFolderName = 'Part one';
+
+const deployDashboard = async (dashboard: DashboardBuilder): Promise<void> => {
+    const grafana = Client.withConfigFromEnv(process.env);
+
+    const folderUid = await grafana.findOrCreateFolder(dashboardFolderName);
+    await grafana.persistDashboard(folderUid, dashboard);
+
+    console.log(`dashboard deployed`);
+};
+
+const generateManifest = async (dashboard: DashboardBuilder): Promise<void> => {
+    const grafana = Client.withConfigFromEnv(process.env);
+    console.log(`grafana host: ${grafana}`);
+    const builtDashboard = dashboard.build();
+
+    if (!fs.existsSync(manifestsDir)) {
+        console.log(`creating ${manifestsDir} directory...`);
+        fs.mkdirSync(manifestsDir);
+    }
+
+    console.log(`creating folder ${dashboardFolderName}...`);
+    const folderUid = await grafana.findOrCreateFolder(dashboardFolderName);
+    console.log(`folder uid: ${folderUid}`);
+    const manifest = dashboardManifest(folderUid, builtDashboard);
+    const manifestYaml = yaml.stringify(JSON.parse(JSON.stringify(manifest)));
+
+    const filename = path.join(manifestsDir, `${builtDashboard.uid!}.yaml`);
+    fs.writeFileSync(filename, manifestYaml);
+
+    console.log(`manifest generated in ${manifestsDir}`);
+};
+
+(async () => {
+    const deploy = process.argv.includes('--deploy');
+    const manifests = process.argv.includes('--manifests');
+    const help = process.argv.includes('--help') || process.argv.includes('-h');
+    
+    if (help) {
+        console.log('Usage:');
+        console.log("\t--deploy\tGenerate and deploy the test dashboard directly to a Grafana instance");
+        console.log("\t--manifests\tGenerate a dashboard manifest for the test dashboard and write it to disk");
+        process.exit(1);
+    }
+
+    const dashboard = exampleDashboard();
+
+    if (deploy) {
+        // Deploy the test dashboard directly to a Grafana instance.
+        await deployDashboard(dashboard);
+        return;
+    }
+    
+    if (manifests) {
+
+        console.log('Generating manifest...');
+        // Generate a dashboard manifest for the test dashboard and write it to disk.
+        await generateManifest(dashboard);
+        return;
+    }
+
+	// By default: print the test dashboard to stdout.
+    const manifest = dashboardManifest('', dashboard.build());
+    const manifestYaml = yaml.stringify(JSON.parse(JSON.stringify(manifest)));
+    console.log(manifestYaml);
+})();
