82986d0dac04d0f88d3fabc8141ac42e3bf5632f
Dominik Prokop <dominik.prokop@grafana.com>
2025-04-29 17:04:31 +0200

    Simpler foundation demo

--- foundation/src/dashboard.ts
+++ foundation/src/dashboard.ts
@@ -2,19 +2,11 @@ import {
     DashboardBuilder,
     DashboardCursorSync,
 } from '@grafana/grafana-foundation-sdk/dashboard';
-import * as common from '@grafana/grafana-foundation-sdk/common';
-import * as logs from '@grafana/grafana-foundation-sdk/logs';
-import * as stat from '@grafana/grafana-foundation-sdk/stat';
-import * as text from '@grafana/grafana-foundation-sdk/text';
 import * as prometheus from '@grafana/grafana-foundation-sdk/prometheus';
+import * as text from '@grafana/grafana-foundation-sdk/text';
 import * as timeseries from '@grafana/grafana-foundation-sdk/timeseries';
 import {
-    logPanel,
-    lokiDatasourceRef,
-    lokiQuery,
     prometheusDatasourceRef,
-    prometheusQuery,
-    statPanel,
     textPanel,
     timeseriesPanel
 } from './common';
@@ -36,53 +28,22 @@ export const exampleDashboard = (): DashboardBuilder => {
     return builder;  
 };
 
-export const prometheusVersionStat = (): stat.PanelBuilder => {
-    return statPanel()
-        .title('Prometheus version')
-        .withTarget(
-            instantPrometheusQuery(`prometheus_build_info{}`)
-        )
-        .transparent(true)
-        .datasource(prometheusDatasourceRef())
-        .reduceOptions(
-            new common.ReduceDataOptionsBuilder()
-                .calcs(['last'])
-                .fields('/^version$/')
-        )
-    ;
-};
+export const grafanaGoroutinesTimeseries = (): timeseries.PanelBuilder => {
+    const query = new prometheus.DataqueryBuilder()
+            .expr(`go_goroutines{job="grafan"}`)
+            .range()
+            .format(prometheus.PromQueryFormat.TimeSeries)
+            .legendFormat('__auto')
 
- const instantPrometheusQuery = (expression: string): prometheus.DataqueryBuilder => {
-    return new prometheus.DataqueryBuilder()
-        .expr(expression)
-        .instant()
-        .format(prometheus.PromQueryFormat.Table)
-        .legendFormat('__auto')
+    return timeseriesPanel()
+        .title('Grafana goroutines')
+        .withTarget(query)
+        .datasource(prometheusDatasourceRef())
     ;
 };
 
 export const descriptionText = (): text.PanelBuilder => {
     return textPanel(`Text panels are supported too! Even with *markdown* text :)`)
         .transparent(true)
-    ;
-};
-
-export const unfilteredLogs = (): logs.PanelBuilder => {
-    return logPanel()
-        .title('Logs')
-        .withTarget(
-            lokiQuery(`{job="app_logs"}`)
-        )
-        .datasource(lokiDatasourceRef())
-    ;
-};
-
-export const grafanaGoroutinesTimeseries = (): timeseries.PanelBuilder => {
-    return timeseriesPanel()
-        .title('Grafana goroutines')
-        .withTarget(
-            prometheusQuery(`go_goroutines{job="grafana"}`)
-        )
-        .datasource(prometheusDatasourceRef())
     ;
 };
